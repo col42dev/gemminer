19beac1918c286caf41ec73ff9708257f0859c8e
Colin Moore <colin.moore42@googlemail.com>
2015-12-10 18:11:00 +0000

    first commit

---
+++ Assets/Scripts/State/StateTransaction.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public interface IStateTransactionMessageTarget : IEventSystemHandler
+{
+    void TransactionMessage(string message);
+    void RequestTransmitState();
+}
+
+public class StateTransaction : MonoBehaviour, IStateTransactionMessageTarget
+{
+    #region Private Members
+    private List<string> mTransactions;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        mTransactions = new List<string>();
+    }
+
+    public void TransactionMessage(string message)
+    {
+        Debug.Log("Message 1 received:" + message);
+        mTransactions.Add(message);
+    }
+
+    public void RequestTransmitState()
+    {
+        // 
+        Debug.Log("Transmiting State");
+
+        string url = "http://localhost:3000/";
+        WWWForm form = new WWWForm();
+
+        string messageBody = "";
+        foreach (string transactionMessage in mTransactions)
+        {
+            messageBody += transactionMessage + '\n';
+        }
+        form.AddField("message", messageBody);
+
+        WWW www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log("WWW Ok!: " + www.text);
+        } else {
+            Debug.Log("WWW Error: "+ www.error);
+        }    
+    } 
+
+}
+
+
---
+++ Assets/Scripts/UI/StateUI.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using SimpleJSON;
+
+public class StateUI : MonoBehaviour
+{
+
+    public GameObject ResourceItem;
+	
+    private bool mPendingPopulate = true;
+    private int mGoldQuantity = 0;
+
+
+
+    void Update()
+    {
+        if (SampleState.Instance != null)
+        {
+            if (mPendingPopulate)
+            {
+                mPendingPopulate = false;
+
+                JSONClass resources = SampleState.Instance.mState["resources"] as JSONClass;
+
+                foreach (KeyValuePair<string, JSONNode> stateObj in resources)
+                {
+                    JSONNode node = stateObj.Value;
+                    string textOut = stateObj.Key + ", Quantity: " + (string)node["quantity"] + ", Cost: " + (string)node["cost"] + "\n";
+                    GameObject test3 = GameObject.Instantiate(ResourceItem);
+                    test3.transform.FindChild("Text").GetComponent<Text>().text = textOut;
+                    test3.transform.SetParent(this.gameObject.transform.FindChild("ResourceListComponent").transform.FindChild("ResourceContainer").transform);
+                }
+            }
+
+            JSONClass gold = SampleState.Instance.mState["gold"] as JSONClass;
+            if ( mGoldQuantity != gold["quantity"].AsInt) {
+                mGoldQuantity = gold["quantity"].AsInt;
+                GameObject.Find("Gold").GetComponent<Text>().text = "Gold: " + mGoldQuantity;
+            }
+
+     
+        }
+    }
+
+    public void HandleBuyRequest(Text textUI)
+    {
+        Debug.Log(textUI.text);
+        Match match = Regex.Match(textUI.text, @"(\S*), .*");
+        if (match.Success)
+        {
+            string resourceName = match.Groups[1].Value.ToString();
+            JSONNode resourceNode = SampleState.Instance.mState["resources"][resourceName];
+            
+            JSONClass gold = SampleState.Instance.mState["gold"] as JSONClass;
+            int newGoldQuantity = gold["quantity"].AsInt - resourceNode["cost"].AsInt;
+            gold["quantity"] = new JSONData(newGoldQuantity);
+    
+            int newQuantity = resourceNode["quantity"].AsInt + 1;
+            resourceNode["quantity"] = new JSONData(newQuantity);
+            textUI.text = resourceName + ", Quantity: " + (string)resourceNode["quantity"] + ", Cost: " + (string)resourceNode["cost"] + "\n";
+            string transactionMessage = "buy 1 " + resourceName;
+            ExecuteEvents.Execute<IStateTransactionMessageTarget>(GameObject.Find("StateTransaction"), null, (x, y) => x.TransactionMessage(transactionMessage));
+        }
+    }
+
+    public  void HandleSellRequest(Text textUI)
+    {
+        Debug.Log(textUI.text);
+        Match match = Regex.Match(textUI.text, @"(\S*), .*");
+        if (match.Success)
+        {
+            string resourceName = match.Groups[1].Value.ToString();
+            JSONNode resourceNode = SampleState.Instance.mState["resources"][resourceName];
+
+            JSONClass gold = SampleState.Instance.mState["gold"] as JSONClass;
+            int newGoldQuantity = gold["quantity"].AsInt + resourceNode["cost"].AsInt;
+            gold["quantity"] = new JSONData(newGoldQuantity);
+
+            int newQuantity = resourceNode["quantity"].AsInt - 1;
+            resourceNode["quantity"] = new JSONData(newQuantity);
+            textUI.text = resourceName + ", Quantity: " + (string)resourceNode["quantity"] + ", Cost: " + (string)resourceNode["cost"] + "\n";
+            string transactionMessage = "sell 1 " + resourceName;
+            ExecuteEvents.Execute<IStateTransactionMessageTarget>(GameObject.Find("StateTransaction"), null, (x, y) => x.TransactionMessage(transactionMessage));
+        }
+    }
+
+
+    public  void HandleTransmitState()
+    {
+        ExecuteEvents.Execute<IStateTransactionMessageTarget>(GameObject.Find("StateTransaction"), null, (x, y) => x.RequestTransmitState());
+    }
+}
